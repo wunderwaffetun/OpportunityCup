f605e53121a117bdff581f14444c4ea0054db36d
wundervaffetun <egor.kor013000@gmail.com>
2022-09-29 18:54:04 +0300

    test_file_to_json

--- py/Script.py
+++ py/Script.py
@@ -1,66 +1,58 @@
 import os
 import json
-from typing import List
+import re
 
 class OperationData:
-    propertyNames = ['values', 'date', 'card', 'account', 'accountValidTo', 'client', 'lastName',
+    propertyNames = ['date', 'card', 'account', 'accountValidTo', 'client', 'lastName',
                         'firstName', 'patronymic', 'dateOfBirth', 'passport', 'passportValidTo', 'phone', 
                         'operType', 'amount', 'operResult', 'terminal', 'terminalType', 'city', 'address']
     def __init__(self, objectValueList):
-        self.values = objectValueList
-        self.date = ''
-        self.card = ''
-        self.account = ''
-        self.accountValidTo = ''
-        self.client = ''
-        self.lastName = ''
-        self.firstName = ''
-        self.patronymic = ''
-        self.dateOfBirth = ''
-        self.passport = ''
-        self.passportValidTo = ''
-        self.phone = ''
-        self.operType = ''
-        self.amount = ''
-        self.operResult = ''
-        self.terminal = ''
-        self.terminalType = ''
-        self.city = ''
-        self.address = ''
-    def set_data(self, array):
-        if len(array) != len(OperationData.propertyNames):
+        self._rank = 20
+        if len(objectValueList) != len(OperationData.propertyNames):
             raise ValueError("Bad data length")
-        for i in range(len(array)):
-            value = array[i]
+        for i in range(len(objectValueList)):
+            value = objectValueList[i]
             if value is not None: 
                 key = OperationData.propertyNames[i]
                 setattr(self, key, value)
+    def toJSON(self):
+        return json.dumps(self, default = lambda o: f"{o.__dict__}", indent = 2)
+    def set_rank(self, rank):
+        self._rank = rank
+    def get_rank(self):
+        return self._rank
     def __str__(self):
-        return "OperationData(%s)" % ','.join([str(getattr(self, key)) for key in OperationData.propertyNames])
+        return "OperationData(%s)" % ','.join([key + ": " + str( getattr(self, key)) for key in OperationData.propertyNames])
     def __repr__(self):
         return str(self)
 
-            
-        
 
 
-jsonFile = open(f'{os.path.dirname(os.getcwd())}/transactions.json', encoding='utf-8')
-jsonObject = json.load(jsonFile)
-objectsList = list()
+def readJsonFile(objectsList = []):
+    jsonFile = open(f'{os.path.dirname(os.getcwd())}/transactions.json', encoding='utf-8')
+    jsonObject = json.load(jsonFile)
 
-for numberObj, DataObject in enumerate(jsonObject["transactions"]):
-    objectKeyList = list()
-    objectValueList = list()
-    for key in jsonObject["transactions"][DataObject]:
-        objectKeyList.append(key)
-        objectValueList.append(jsonObject["transactions"][DataObject][key])
-        print(key, jsonObject["transactions"][DataObject][key]) 
-    objectsList.append(OperationData(objectValueList))
-    break
-print(objectsList)
+    for numberObj, DataObject in enumerate(jsonObject["transactions"]):
+        objectValueList = []
+        for key in jsonObject["transactions"][DataObject]:
+            objectValueList.append(jsonObject["transactions"][DataObject][key])
+        objectsList.append(OperationData(objectValueList))
+    jsonFile.close()
+    return objectsList
+
+def globalFilters(objectsList):
+    with open('./testFile.txt', 'w+', encoding = 'utf-8') as output:
+        counter = 0
+        for object in objectsList:
+            if object.operResult == 'Отказ':
+                JSON = json.dumps(object.toJSON())
+                JSON = re.sub(r',', lambda o: ',\n', str(object))
+                output.write(JSON + '\r\n')
 
 
+def __main__():
+    objectsList = readJsonFile([])
+    globalFilters(objectsList)
+    
 
-
-
-jsonFile.close()
+__main__()
